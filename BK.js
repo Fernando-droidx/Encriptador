60db5330b1deb697a8ad25749b6abe94a5de3cce
Fernando <NORMA@DESKTOP-4V8SPNP>
2023-04-19 23:02:05 -0600

    cambios: boton de limpiar, y pasa de un lado a ootro el string

--- BK.js
+++ BK.js
@@ -1,3 +1,20 @@
+let campoTexto;
+let resultado;
 function Hola(saludo){
     alert(saludo);
+}
+
+
+function encriptador() {
+    campoTexto = document.getElementById("entrada");
+    resultado = document.getElementById("resultado");
+    resultado.value = campoTexto.value;
+}
+
+function limpiar(){
+    campoTexto.value=" ";
+    resultado.value = " ";
+
+    
+    
 }
