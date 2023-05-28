ad332324289daa426a8147a55f20584f9119a487
Luis Fernando Ramírez Rivas <114302687+Fernando-droidx@users.noreply.github.com>
2023-05-28 17:25:40 -0600

    j

--- BK.js
+++ BK.js
@@ -1,33 +1,37 @@
-let campoTexto;
-let resultado;
-let enc;
+
+
+
 
 function Hola(saludo){
     alert(saludo);
 }
+function encriptar(){
+   
+    var frase = document.getElementById("inputText").value.toLowerCase();
+    var textoEncriptado = frase.replace(/e/img, "enter");
+    textoEncriptado = textoEncriptado.replace(/o/img, "ober");
+    textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+    textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+    textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
 
-
-function encriptador() {
-    campoTexto = document.getElementById("entrada");
-    resultado = document.getElementById("resultado");
-    
-    campoTexto.value = resultado.value;
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
     
     
 }
+
 //TODO:el boton tiene que ser capaz de desencriptar 
 
 function desencriptador(){
-    resultado = document.getElementById("resultado");
-    campoTexto = document.getElementById("entrada");
+   
+   
 
-    resultado.value = campoTexto.value.tolowerCase();
+   
 } 
 //TODO: hacer una funcion que copie el texto encriptado
 function copiar(){
     
 }
 function limpiar(){
-    campoTexto.value="";
-    resultado.value = "";  
+    
+   
 }
